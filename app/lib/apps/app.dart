46f15dfd69126d425cfb6d3f448a5c52266181f2
Suman Adhikari <expertxkid@gmail.com>
2021-11-09 22:33:58 +0545

    Restructure app for material design

---
+++ app/lib/apps/app.dart
@@ -0,0 +1,18 @@
+import 'package:app/screens/home.dart';
+import 'package:app/themes/dark_theme.dart';
+import 'package:app/themes/light_theme.dart';
+import 'package:flutter/material.dart';
+
+class MyMaterialApp extends StatelessWidget {
+  const MyMaterialApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.light,
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ app/lib/themes/light_theme.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(brightness: Brightness.light);
