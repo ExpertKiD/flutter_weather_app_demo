630713b9e9b5ac68e1470a9296268823500698b8
Suman Adhikari <expertxkid@gmail.com>
2021-11-11 09:55:56 +0545

    Update colors in lightTheme

--- app/lib/themes/light_theme.dart
+++ app/lib/themes/light_theme.dart
@@ -1,3 +1,12 @@
+import 'package:app/resources/colors.dart';
 import 'package:flutter/material.dart';
 
-final lightTheme = ThemeData(brightness: Brightness.light);
+final lightTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: AppColors.sunshineBlue,
+  primaryColorLight: AppColors.sunshineLightBlue,
+  primaryColorDark: AppColors.sunshineDarkBlue,
+  appBarTheme: const AppBarTheme(
+    backgroundColor: AppColors.sunshineBlue,
+  ),
+);
