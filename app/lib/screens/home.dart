96cd70c7e3ba6af182842309648bb521d66ba628
Suman Adhikari <expertxkid@gmail.com>
2021-11-11 09:39:35 +0545

    Add basic layout for home screen

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -1,4 +1,6 @@
+import 'package:app/resources/colors.dart';
 import 'package:app/screens/settings.dart';
+import 'package:app/screens/weather_detail.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,25 +9,69 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Image.asset(
-          'assets/images/ic_logo.png',
-          // cacheWidth: 256,
-          cacheHeight: 48,
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SettingsScreen()),
-              );
-            },
-            icon: const Icon(Icons.more_vert),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: AppColors.sunshineBlue,
+            pinned: true,
+            snap: false,
+            floating: false,
+            expandedHeight: 300,
+            actions: [
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SettingsScreen()),
+                  );
+                },
+                icon: const Icon(Icons.more_vert),
+              ),
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              background: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(height: 300 - 56, color: Colors.red)),
+            ),
+            title: Image.asset(
+              'assets/images/ic_logo.png',
+              cacheHeight: 48,
+            ),
           ),
+          const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 20,
+            ),
+          ),
+          SliverList(
+            delegate:
+                SliverChildBuilderDelegate((BuildContext context, int index) {
+              return Material(
+                color: index % 2 == 0
+                    ? Colors.blue.shade200
+                    : Colors.blue.shade600,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => WeatherDetailScreen(),
+                      ),
+                    );
+                  },
+                  child: Center(
+                    child: Text(
+                      '${index + 1}',
+                      textScaleFactor: 5,
+                    ),
+                  ),
+                ),
+              );
+            }, childCount: 20),
+          )
         ],
       ),
-      body: Container(),
     );
   }
 }
