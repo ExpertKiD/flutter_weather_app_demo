27a77f73fc7054309d74803e50978b166858a1d2
Suman Adhikari <expertxkid@gmail.com>
2021-11-11 09:39:07 +0545

    Add colors and strings class

---
+++ app/lib/resources/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const white = Color(0xffffffff);
+  static const grey = Color(0xffcccccc);
+  static const grey_700 = Color(0xff646464);
+  static const black = Color(0xff000000);
+  static const sunshineLightBlue = Color(0xff64c2f4);
+  static const sunshineBlue = Color(0xff1ca8f4);
+  static const sunshineDarkBlue = Color(0xff0288D1);
+
+  AppColors._internal();
+}
