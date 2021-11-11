16855157f5a46d0928798a33794a27427aee84c8
Suman Adhikari <expertxkid@gmail.com>
2021-11-11 21:22:02 +0545

    Add dailyForecasts to OneCallWeatherData model

--- app/lib/models/one_call_weather_data.dart
+++ app/lib/models/one_call_weather_data.dart
@@ -1,24 +1,46 @@
 import 'dart:convert';
 
+import 'package:app/models/weather.dart';
+
 class OneCallWeatherData {
   final double latitude;
   final double longitude;
   final String timezone;
   final int timezoneOffset;
+  final List<Weather> dailyForecasts;
 
   OneCallWeatherData({
     required this.latitude,
     required this.longitude,
     required this.timezone,
     required this.timezoneOffset,
+    required this.dailyForecasts,
   });
 
   factory OneCallWeatherData.fromJSON(Map<String, dynamic> json) {
+    List<Weather> dailyForecastList =
+        (json["daily"] as List<dynamic>).map((weatherJson) {
+      return Weather(
+        dt: weatherJson["dt"] as int,
+        tempMin: weatherJson["temp"]["min"] as double,
+        tempMax: weatherJson["temp"]["max"] as double,
+        weatherCondition: weatherJson["weather"][0]["main"],
+        humidity: weatherJson["humidity"] as int,
+        pressure: weatherJson["pressure"] as int,
+        windSpeed: weatherJson["wind_speed"] as double,
+        windDirection: weatherJson["wind_deg"] as int,
+      );
+    }).toList();
+
+    // removing today's forecast
+    dailyForecastList.removeAt(0);
+
     return OneCallWeatherData(
       latitude: json["lat"] as double,
       longitude: json["lon"] as double,
       timezone: json["timezone"].toString(),
       timezoneOffset: json["timezone_offset"] as int,
+      dailyForecasts: dailyForecastList,
     );
   }
 
