c080bc377157eeb5a949474a9fe75ea07f0cd6bc
Suman Adhikari <expertxkid@gmail.com>
2021-11-11 23:41:41 +0545

    Add const modifers as per dart analysis

--- app/lib/screens/weather_detail.dart
+++ app/lib/screens/weather_detail.dart
@@ -1,17 +1,25 @@
+import 'package:app/models/weather.dart';
 import 'package:flutter/material.dart';
 
 class WeatherDetailScreen extends StatelessWidget {
-  const WeatherDetailScreen({Key? key}) : super(key: key);
+  final Weather weather;
+
+  const WeatherDetailScreen({
+    Key? key,
+    required this.weather,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Weather Details'),
+        title: const Text('Weather Details'),
       ),
-      body: Center(
-        child:
-            Text('This page shows the weather for the selected day in detail.'),
+      body: Column(
+        children: const [
+          Text('This page shows the weather for the selected day in detail.'),
+          Text('This page shows the weather for the selected day in detail.'),
+        ],
       ),
     );
   }
