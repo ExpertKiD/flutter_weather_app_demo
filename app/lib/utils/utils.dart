bd3e22e9f5d6a315f0d700a909e047009ffc5ad6
Suman Adhikari <expertxkid@gmail.com>
2021-11-11 23:55:46 +0545

    Remove this keyword

--- app/lib/utils/utils.dart
+++ app/lib/utils/utils.dart
@@ -1,6 +1,6 @@
 extension StringFormat on double {
   String toStringAsFlexibleFraction({int fractionalDigits = 0}) {
-    return this.toStringAsFixed(
-        this.truncateToDouble() == this.round() ? 0 : fractionalDigits);
+    return toStringAsFixed(
+        truncateToDouble() == round() ? 0 : fractionalDigits);
   }
 }
