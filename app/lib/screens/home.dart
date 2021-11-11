be41cffb14c3055708cbf48e43217842e328fb06
Suman Adhikari <expertxkid@gmail.com>
2021-11-12 00:48:27 +0545

    Complete UI widget for current weather on home screen

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -2,10 +2,13 @@ import 'package:app/models/current_weather_data.dart';
 import 'package:app/models/one_call_weather_data.dart';
 import 'package:app/models/weather_data.dart';
 import 'package:app/providers/weather_provider.dart';
+import 'package:app/resources/colors.dart';
 import 'package:app/screens/settings.dart';
-import 'package:app/screens/weather_detail.dart';
+import 'package:app/widgets/weather_tile.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -26,7 +29,7 @@ class HomeScreen extends ConsumerWidget {
                 pinned: true,
                 snap: false,
                 floating: false,
-                expandedHeight: 300,
+                expandedHeight: 290,
                 actions: [
                   IconButton(
                     onPressed: () {
@@ -42,52 +45,121 @@ class HomeScreen extends ConsumerWidget {
                 flexibleSpace: FlexibleSpaceBar(
                   background: Align(
                       alignment: Alignment.bottomCenter,
-                      child: Container(height: 300 - 56, color: Colors.red)),
+                      child: Container(
+                        height: 290 - 56,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              flex: 7,
+                              child: Container(
+                                margin: const EdgeInsets.only(
+                                    top: 32, bottom: 16, left: 60),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Today, April 20',
+                                      style: GoogleFonts.robotoCondensed(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.normal,
+                                        color: AppColors.white,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 16),
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.baseline,
+                                        textBaseline: TextBaseline.alphabetic,
+                                        children: [
+                                          Text(
+                                            '16\u00b0',
+                                            style: GoogleFonts.robotoCondensed(
+                                              fontSize: 72,
+                                              fontWeight: FontWeight.w100,
+                                              color: AppColors.white,
+                                            ),
+                                          ),
+                                          Text(
+                                            '4\u00b0',
+                                            style: GoogleFonts.robotoCondensed(
+                                              fontSize: 36,
+                                              fontWeight: FontWeight.w100,
+                                              color: AppColors.white,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Text(
+                                      'London, UK',
+                                      style: GoogleFonts.robotoCondensed(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.normal,
+                                        color: AppColors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 5,
+                              child: Container(
+                                margin: EdgeInsets.only(right: 16),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Image.asset(
+                                      'assets/images/art_clear.png',
+                                      cacheWidth: 122,
+                                    ),
+                                    Container(
+                                      margin: EdgeInsets.only(top: 16),
+                                      child: Text(
+                                        'Clear',
+                                        style: GoogleFonts.robotoCondensed(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.normal,
+                                          color: AppColors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
                 ),
                 title: Image.asset(
                   'assets/images/ic_logo.png',
                   cacheHeight: 48,
                 ),
               ),
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 20,
-                ),
-              ),
               SliverList(
                 delegate: SliverChildBuilderDelegate(
                     (BuildContext context, int index) {
-                  return Material(
-                    color: index % 2 == 0
-                        ? Colors.blue.shade200
-                        : Colors.blue.shade600,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => WeatherDetailScreen(),
-                          ),
-                        );
-                      },
-                      child: Center(
-                        child: Text(
-                          '${index + 1}',
-                          textScaleFactor: 5,
-                        ),
-                      ),
-                    ),
-                  );
-                }, childCount: 20),
+                  return WeatherTile(
+                      weather:
+                          weatherData.oneCallWeatherData.dailyForecasts[index]);
+                },
+                    childCount:
+                        weatherData.oneCallWeatherData.dailyForecasts.length),
               )
             ],
           );
         },
         error: (error, stackTrace) {
-          return Center(child: Text('Error'));
+          return const Center(child: Text('Error'));
         },
         loading: () {
-          return Center(child: CircularProgressIndicator());
+          return const Center(child: CircularProgressIndicator());
         },
       ),
     );
