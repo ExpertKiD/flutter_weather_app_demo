79ee383f615a723837d8407c2c11802da1a5bfbd
Suman Adhikari <expertxkid@gmail.com>
2021-11-11 11:22:50 +0545

    Add ProviderScope parent to MaterialApp()

--- app/lib/apps/app.dart
+++ app/lib/apps/app.dart
@@ -2,17 +2,19 @@ import 'package:app/screens/home.dart';
 import 'package:app/themes/dark_theme.dart';
 import 'package:app/themes/light_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MyMaterialApp extends StatelessWidget {
   const MyMaterialApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ProviderScope(
+        child: MaterialApp(
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: ThemeMode.light,
       home: const HomeScreen(),
-    );
+    ));
   }
 }
