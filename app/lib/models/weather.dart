7d2c531b94fc0c222ec0152c408c2a19f27a79ff
Suman Adhikari <expertxkid@gmail.com>
2021-11-12 10:01:19 +0545

    Add getWeatherCondition() method

--- app/lib/models/weather.dart
+++ app/lib/models/weather.dart
@@ -1,5 +1,15 @@
 import 'package:intl/intl.dart';
 
+enum WeatherCondition {
+  clear,
+  clouds,
+  fog,
+  lightRain,
+  rain,
+  snow,
+  storm,
+}
+
 class Weather {
   final int dt;
   final double tempMin;
@@ -25,16 +35,35 @@ class Weather {
 
   String getDay() {
     DateTime targetDateTime = DateTime.fromMillisecondsSinceEpoch(dt * 1000);
-
     DateTime currentDateTime = DateTime.now();
-
-    print('Current Day:' + currentDateTime.day.toString());
-    print('Target Day:' + targetDateTime.day.toString());
 
     if (targetDateTime.day - currentDateTime.day == 1) {
       return 'Tomorrow';
     } else {
       return DateFormat('EEEE').format(targetDateTime).toString();
     }
+  }
+
+  WeatherCondition getWeatherCondition() {
+    WeatherCondition currentCondition = WeatherCondition.clear;
+
+    if (weatherConditionId >= 200 && weatherConditionId <= 232) {
+      currentCondition = WeatherCondition.storm;
+    } else if ((weatherConditionId >= 300 && weatherConditionId <= 321) ||
+        weatherConditionId == 500) {
+      currentCondition = WeatherCondition.lightRain;
+    } else if (weatherConditionId >= 500 && weatherConditionId <= 531) {
+      currentCondition = WeatherCondition.rain;
+    } else if (weatherConditionId >= 600 && weatherConditionId <= 622) {
+      currentCondition = WeatherCondition.snow;
+    } else if (weatherConditionId >= 701 && weatherConditionId <= 781) {
+      currentCondition = WeatherCondition.fog;
+    } else if (weatherConditionId == 800) {
+      currentCondition = WeatherCondition.clear;
+    } else if (weatherConditionId >= 801 && weatherConditionId <= 804) {
+      currentCondition = WeatherCondition.clouds;
+    }
+
+    return currentCondition;
   }
 }
